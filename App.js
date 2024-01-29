529c48456ddd6ebafebe5b86c80ec8de9ef50c93
JagGhost <simijani2000@gmail.com>
2024-01-29 18:33:58 +0100

    kaki

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#EAEAE8" }}>
+      <SafeAreaView>
+        <View style={{ paddingHorizontal: 20 }}>
+          <View style={{ justifyContent: "space-between", paddingTop: 25 }}>
+            <View style={{ backgroundColor: "grey"}}>
+              <Text style={{ fontSize: 25 }}>Welcome back</Text>
+              <Text style={{ fontSize: 25, color: "black", fontWeight: "bold" }}>Simicsák János{"\n"}</Text>
+              <View style={{ width: 48, height: 48, backgroundColor: "#F68827", borderRadius: 12, alignSelf: "flex-end" }}></View>
+            </View>
+            <View style={{ width: 340, height: 210, backgroundColor: "#1AA0B9", borderRadius: 12, alignSelf: "center", justifyContent: "center" }}>
+              <Text style={{ alignSelf: "center" }}>Img here</Text>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
